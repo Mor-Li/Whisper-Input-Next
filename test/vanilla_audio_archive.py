dacb724f0b07c057ead32ed8fadb0c17bb1536e1
Mor-Li <2568818204@qq.com>
2025-09-16 01:32:13 +0800

    update openai official api test script

--- test/vanilla_audio_archive.py
+++ test/vanilla_audio_archive.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 client = OpenAI()
 
-with open("audio_archive/recording_20250913_073924.wav", "rb") as f:
+# openai 官方api测试
+with open("audio_archive/recording_20250916_010510.wav", "rb") as f:
     resp = client.audio.transcriptions.create(
         model="gpt-4o-transcribe",   # 或 "gpt-4o-mini-transcribe"
         file=f,                      # 关键点：传二进制文件对象
@@ -9,3 +10,7 @@ with open("audio_archive/recording_20250913_073924.wav", "rb") as f:
         # temperature=0,             # 可选
     )
 print(resp.text)
+
+# proxy_on
+# unset OPENAI_BASE_URL
+# export OPENAI_API_KEY=$OFFICIAL_OPENAI_API_KEY
